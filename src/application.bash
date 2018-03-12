f3e51256baa79cbd92a8a650e7c5a96e3039290e
Vincent Beucher <vincent.beucher@ets-conty.fr>
2018-03-12 13:50:50 +0100

    display copyright

--- src/application.bash
+++ src/application.bash
@@ -1,3 +1,4 @@
-###Application.bash - 201x - BEUCHER Vincent ###
-echo "version 1"
+### Application.bash - 201x - BEUCHER Vincent ###
+echo "version 1.1"
+echo "Copyright Company - 201x"
 echo "Build : 10:00"
